4f107885347b12f47f1a9a81fac493409f7365c4
Abin <ebyavarachan@gmail.com>
2024-07-15 23:08:50 +0530

    gamegrid changes last commit

--- src/components/GamesGrid.tsx
+++ src/components/GamesGrid.tsx
@@ -6,34 +6,33 @@ import GameCardContainer from "./GameCardContainer";
 import { GameQuery } from "../App";
 
 interface Props {
-  gameQuery:GameQuery
+  gameQuery: GameQuery;
 }
 
 const GamesGrid = ({ gameQuery }: Props) => {
   const { data, error, isloading } = useGames(gameQuery);
   const skelton = [1, 2, 3, 4, 5, 6];
 
+  if (error) return <Text>{error}</Text>;
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={6}
-        padding="10px"
-      >
-        {isloading &&
-          skelton.map((skel) => (
-            <GameCardContainer key={skel}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      spacing={6}
+      padding="10px"
+    >
+      {isloading &&
+        skelton.map((skel) => (
+          <GameCardContainer key={skel}>
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
